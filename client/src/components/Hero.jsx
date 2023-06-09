1f6d3763b0401944ac733fc4ecca1e4fc10a5932
shio-is-salty <kylepagayon58@gmail.com>
2023-06-09 09:32:33 +0800

    download resume function added

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import meImg from "../assets/me.jpg"
 import {BsDownload, BsChevronDoubleDown} from "react-icons/bs"
+import myResume from "../assets/kyle-pagayon-resume.pdf"
 const Hero = () => {
     return (
         <section id="home" className="py-16 pb-32 ">
@@ -12,11 +13,14 @@ const Hero = () => {
                     <h1 className="text-6xl">Kyle  Pagayon</h1>
                     <h1 className="text-xl font-bold">Junior full stack web developer</h1>
                     <p className="text-sm leading-relaxed">I am a junior full stack developer I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.</p>
-                    <button className="mt-4 flex gap-2 items-center border border-white p-4 text-white self-start 
+                    <a 
+                      href={myResume}
+                      download
+                      className="mt-4 flex gap-2 items-center border border-white p-4 text-white self-start 
                     hover:border-green-500 hover:bg-green-500 hover:text-black transition-all duration-300">
                         <BsDownload />
                         Download CV/Resume
-                    </button>
+                    </a>
                 </div>
 
                 <div className="hidden md:block max-w-[360px] max-h-[360px] rounded-full shadow-gray-600  shadow-sm">
